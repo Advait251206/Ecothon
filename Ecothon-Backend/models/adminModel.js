468a4cf729f197e595c899d565493811d70dc0db
Advait Kawale <advaitkawale@gmail.com>
2026-06-01 20:35:29 +0530

    chore: remove legacy Ecothon-Backend folder

--- Ecothon-Backend/models/adminModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = mongoose.Schema({
-    username: { type: 'string', required: true, unique: true},
-    email: { type: 'string', required: true, unique: true},
-    password: { type: 'string', required: true },
-});
-const Users = mongoose.model('Users', userSchema);
-export default Users;
